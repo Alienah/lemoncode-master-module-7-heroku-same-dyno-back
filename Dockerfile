e2f41858090acb0c7de43c2560314e98fbabbe4b
Aida Albarrán Blanco <aalbarranbla@vmware.com>
2022-09-19 20:35:07 +0200

    Add config to deploy front and back in the same dyno

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,22 @@ FROM node:16-alpine AS base
 RUN mkdir -p /usr/app
 WORKDIR /usr/app
 
+FROM base AS build-front
+ARG BASE_API_URL
+ENV BASE_API_URL=$BASE_API_URL
+ARG FRONT_PATH
+COPY $FRONT_PATH ./
+RUN npm ci
+RUN npm run build
+
 FROM base AS build-backend
 COPY ./ ./
 RUN npm ci
 RUN npm run build
 
 FROM base AS release
+ENV STATIC_FILES_PATH=./public
+COPY --from=build-front /usr/app/dist $STATIC_FILES_PATH
 COPY --from=build-backend /usr/app/dist ./
 COPY ./package.json ./
 COPY ./package-lock.json ./
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import express from 'express';
+import path from 'path';
 import { envConstants } from 'core/constants';
 import { createApp, connectToDBServer } from 'core/servers';
 import { memberApi } from 'pods/member';
@@ -5,6 +7,9 @@ import { memberApi } from 'pods/member';
 const app = createApp();
 
 app.use('/members', memberApi);
+
+const staticFilesPath = path.resolve(__dirname, envConstants.STATIC_FILES_PATH);
+app.use('/', express.static(staticFilesPath));
 
 app.listen(envConstants.PORT, async () => {
   await connectToDBServer(envConstants.MONGODB_URI);
